6d7d22485bb5d75b99487928053c9169b47dbca3
lleite23 <leandrohenrique@frwk.com.br>
2023-03-14 14:18:41 -0300

    Rotas aninhadas

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from './components/Inicio.vue'
+
 import Usuario from './components/usuario/Usuario.vue'
+import UsuarioLista from './components/usuario/UsuarioLista.vue'
+import UsuarioDetalhe from './components/usuario/UsuarioDetalhe.vue'
+import UsuarioEditar from './components/usuario/UsuarioEditar.vue'
 
 
 Vue.use(Router)
@@ -12,8 +16,13 @@ export default new Router({
         path: '/', 
         component: Inicio
     }, {
-        path: '/usuario/:id',
+        path: '/usuario',
         component: Usuario,
         props: true,
+        children: [
+            { path: '', component: UsuarioLista },
+            { path: ':id', component: UsuarioDetalhe, props: true },
+            { path: ':id/editar', component: UsuarioEditar, props: true },
+        ]
     }]
 })
